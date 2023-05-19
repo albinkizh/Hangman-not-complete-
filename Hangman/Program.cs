487c4310ca7d8fdd8ae744d71d5711955db600b9
Albin <Albin@DESKTOP-2LCI1U3>
2023-05-19 09:07:31 +0200

    Projektdateien hinzufügen.

---
+++ Hangman/Program.cs
@@ -0,0 +1,43 @@
+﻿
+var Words = new List<string> {"Baum", "Fortnite"};
+int index = Random.Shared.Next(Words.Count);
+
+string un = "_";
+string bconv;
+do
+{
+
+    Console.WriteLine("Guess a the Words");
+    string guess = Console.ReadLine();
+
+    Console.Clear();
+
+    foreach (char b in Words[index])
+    {
+
+        bconv = Convert.ToString(b);
+
+
+        if (guess != bconv)
+        {
+            Console.Write(un);
+        }
+
+        else
+        {
+            Console.Write(guess);
+        }
+
+        
+    }
+
+
+
+    Console.WriteLine("\n");
+ 
+} while (true);
+
+
+
+
+
